bfb2df01dff5e52da947019c08015377dd4c4984
HyperNatho <me@n-ath.co>
2015-01-30 18:17:40 +1030

    Create project base. (With Maven)

---
+++ src/main/java/me/hypernatho/playerdataremover/PlayerDataRemover.java
@@ -0,0 +1,17 @@
+package me.hypernatho.playerdataremover;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlayerDataRemover extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //onEnable
+    }
+
+    @Override
+    public void onDisable() {
+        //onDisable
+    }
+
+}
